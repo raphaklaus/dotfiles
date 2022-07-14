03f8dadff293f32cee36da62154a2416c8819f06
Raphael D. Pinheiro <raphaklaus@gmail.com>
2022-07-15 00:15:30 +0200

    Organize export/import files

--- export_files.sh
+++ export_files.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+cp .zshrc ~/
+cp .tool-versions ~/
+cp .psqlrc ~/
+cp .gitconfig ~/
 cp .tmux.conf ~/
 cp init.vim ~/.config/nvim
--- import_files.sh
+++ import_files.sh
@@ -5,3 +5,4 @@ cp ~/.config/nvim/init.vim .
 cp ~/.psqlrc .
 cp ~/.gitconfig .
 cp ~/.tool-versions .
+cp ~/.zshrc .
